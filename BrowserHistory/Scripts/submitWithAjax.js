ca5b77f59b9ce576cee7c20a69377b12cee9849b
Simon Bader <simonbader@yahoo.com>
2017-05-05 06:44:02 +0200

    Initial commit

---
+++ BrowserHistory/Scripts/submitWithAjax.js
@@ -0,0 +1,16 @@
+﻿console.info('run submitWithAjax.js');
+console.debug(window.location);
+
+var frm = $('#childForm');
+frm.submit(function (ev) {
+    console.info('run preventDefault');
+    ev.preventDefault();
+    $.ajax({
+        type: frm.attr('method'),
+        url: frm.attr('action'),
+        data: frm.serialize(),
+        success: function (content) {
+            $("html").html(content);
+        }
+    });
+});
